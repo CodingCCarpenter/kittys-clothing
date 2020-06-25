29f5fc64db478197f96bc753abaa04c8c8889f57
CodingCCarpenter <Christineassists@aol.com>
2020-06-24 22:58:38 -0500

    fleshing out component entirely inside of index.js

--- src/components/homepage/Homepage.component.jsx
+++ src/components/homepage/Homepage.component.jsx
@@ -2,6 +2,45 @@ import React from 'react'
 
 const Homepage = () => (
     <div className='homepage'>
-        
-    </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Hats</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Hats</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Hats</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Hats</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        </div>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Hats</h1>
+                        <span className='subtitle'>Shop Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
 )
