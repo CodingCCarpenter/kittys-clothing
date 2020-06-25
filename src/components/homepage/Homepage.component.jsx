92ad8b2329f9534772f9a9e4181c0cd440366883
CodingCCarpenter <Christineassists@aol.com>
2020-06-24 23:01:26 -0500

    correcting error in fleshing out of components within app.js

--- src/components/homepage/Homepage.component.jsx
+++ src/components/homepage/Homepage.component.jsx
@@ -9,40 +9,32 @@ const Homepage = () => (
                     <span className='subtitle'>Shop Now</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>Hats</h1>
                     <span className='subtitle'>Shop Now</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>Hats</h1>
                     <span className='subtitle'>Shop Now</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>Hats</h1>
                     <span className='subtitle'>Shop Now</span>
                 </div>
             </div>
-        </div>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>Hats</h1>
-                        <span className='subtitle'>Shop Now</span>
-                    </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Hats</h1>
+                    <span className='subtitle'>Shop Now</span>
                 </div>
-            </div>
+            </div>  
         </div>
+    </div>
 )
 
 export default Homepage
