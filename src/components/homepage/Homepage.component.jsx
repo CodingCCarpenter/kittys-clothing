42ad1f08c9757abc7bb5e893a0c814177d224c14
CodingCCarpenter <Christineassists@aol.com>
2020-06-24 23:03:18 -0500

    update subtitles to represent each of our 5 menu items

--- src/components/homepage/Homepage.component.jsx
+++ src/components/homepage/Homepage.component.jsx
@@ -11,25 +11,25 @@ const Homepage = () => (
             </div>
             <div className='menu-item'>
                 <div className='content'>
-                    <h1 className='title'>Hats</h1>
+                    <h1 className='title'>Jackets</h1>
                     <span className='subtitle'>Shop Now</span>
                 </div>
             </div>
             <div className='menu-item'>
                 <div className='content'>
-                    <h1 className='title'>Hats</h1>
+                    <h1 className='title'>Sneakers</h1>
                     <span className='subtitle'>Shop Now</span>
                 </div>
             </div>
             <div className='menu-item'>
                 <div className='content'>
-                    <h1 className='title'>Hats</h1>
+                    <h1 className='title'>Women's</h1>
                     <span className='subtitle'>Shop Now</span>
                 </div>
             </div>
             <div className='menu-item'>
                 <div className='content'>
-                    <h1 className='title'>Hats</h1>
+                    <h1 className='title'>Men's</h1>
                     <span className='subtitle'>Shop Now</span>
                 </div>
             </div>  
